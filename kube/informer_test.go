6fb18b625a695c6c79c151c52742ff5a07833948
dmitry <dmitry@mydecisive.ai>
2025-06-30 17:32:46 -0400

    add k8s informer module: initial commit

---
+++ kube/informer_test.go
@@ -0,0 +1,202 @@
+package kube
+
+import (
+	"context"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+)
+
+func TestNewConfigMapController_SingleNs(t *testing.T) {
+	var logger *zap.Logger
+	ctx := context.TODO()
+
+	configMap1 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mdaihub-first-manual-variables",
+			Namespace: "first",
+			Labels: map[string]string{
+				configMapTypeLabel: manualEnvConfigMapType,
+				LabelMdaiHubName:   "mdaihub-first",
+			},
+		},
+		Data: map[string]string{
+			"first_manual_variable": "boolean",
+		},
+	}
+	configMap2 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mdaihub-second-manual-variables",
+			Namespace: "second",
+			Labels: map[string]string{
+				configMapTypeLabel: manualEnvConfigMapType,
+				LabelMdaiHubName:   "mdaihub-second",
+			},
+		},
+		Data: map[string]string{
+			"second_manual_variable": "string",
+		},
+	}
+	configMap3 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mdaihub-third-manual-variables",
+			Namespace: "second",
+			Labels: map[string]string{
+				configMapTypeLabel: manualEnvConfigMapType,
+				LabelMdaiHubName:   "mdaihub-third",
+			},
+		},
+		Data: map[string]string{
+			"third_manual_variable": "string",
+		},
+	}
+
+	clientset := fake.NewClientset(configMap1, configMap2, configMap3)
+
+	cmController, err := NewConfigMapController(manualEnvConfigMapType, "second", clientset, logger)
+	if err != nil {
+		logger.Fatal("failed to create ConfigMap controller", zap.Error(err))
+	}
+
+	stop := make(chan struct{})
+	defer close(stop)
+	err = cmController.Run(stop)
+	if err != nil {
+		logger.Fatal("failed to run ConfigMap controller", zap.Error(err))
+	}
+
+	_, _ = clientset.CoreV1().ConfigMaps("first").Create(context.TODO(), configMap1, metav1.CreateOptions{})
+	_, _ = clientset.CoreV1().ConfigMaps("second").Create(context.TODO(), configMap2, metav1.CreateOptions{})
+	_, _ = clientset.CoreV1().ConfigMaps("second").Create(context.TODO(), configMap3, metav1.CreateOptions{})
+	time.Sleep(100 * time.Millisecond)
+
+	// List ConfigMaps
+	configMaps, err := clientset.CoreV1().ConfigMaps("second").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		t.Fatalf("Failed to list ConfigMaps: %v", err)
+	}
+	assert.Equal(t, 2, len(configMaps.Items))
+
+	cmController.lock.RLock()
+	defer cmController.lock.RUnlock()
+
+	indexer := cmController.cmInformer.Informer().GetIndexer()
+	hubNames := indexer.ListIndexFuncValues(ByHub)
+	sort.Strings(hubNames)
+	assert.Equal(t, hubNames, []string{"mdaihub-second", "mdaihub-third"})
+
+	hubMap := make(map[string]any)
+	for _, hubName := range hubNames {
+		objs, err := indexer.ByIndex(ByHub, hubName)
+		if err != nil {
+			continue
+		}
+		for _, obj := range objs {
+			cm := obj.(*corev1.ConfigMap)
+			hubMap[hubName] = cm
+		}
+	}
+	assert.Equal(t, hubMap["mdaihub-second"], configMap2)
+	assert.Equal(t, hubMap["mdaihub-third"], configMap3)
+
+}
+func TestNewConfigMapController_MultipleNs(t *testing.T) {
+	var logger *zap.Logger
+	ctx := context.TODO()
+
+	configMap1 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mdaihub-first-manual-variables",
+			Namespace: "first",
+			Labels: map[string]string{
+				configMapTypeLabel: manualEnvConfigMapType,
+				LabelMdaiHubName:   "mdaihub-first",
+			},
+		},
+		Data: map[string]string{
+			"first_manual_variable": "boolean",
+		},
+	}
+	configMap2 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mdaihub-second-manual-variables",
+			Namespace: "second",
+			Labels: map[string]string{
+				configMapTypeLabel: manualEnvConfigMapType,
+				LabelMdaiHubName:   "mdaihub-second",
+			},
+		},
+		Data: map[string]string{
+			"second_manual_variable": "string",
+		},
+	}
+	configMap3 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "mdaihub-third-manual-variables",
+			Namespace: "second",
+			Labels: map[string]string{
+				configMapTypeLabel: manualEnvConfigMapType,
+				LabelMdaiHubName:   "mdaihub-third",
+			},
+		},
+		Data: map[string]string{
+			"third_manual_variable": "string",
+		},
+	}
+
+	clientset := fake.NewClientset(configMap1, configMap2, configMap3)
+
+	cmController, err := NewConfigMapController(manualEnvConfigMapType, corev1.NamespaceAll, clientset, logger)
+	if err != nil {
+		logger.Fatal("failed to create ConfigMap controller", zap.Error(err))
+	}
+
+	stop := make(chan struct{})
+	defer close(stop)
+	err = cmController.Run(stop)
+	if err != nil {
+		logger.Fatal("failed to run ConfigMap controller", zap.Error(err))
+	}
+
+	_, _ = clientset.CoreV1().ConfigMaps("first").Create(context.TODO(), configMap1, metav1.CreateOptions{})
+	_, _ = clientset.CoreV1().ConfigMaps("second").Create(context.TODO(), configMap2, metav1.CreateOptions{})
+	_, _ = clientset.CoreV1().ConfigMaps("second").Create(context.TODO(), configMap3, metav1.CreateOptions{})
+	time.Sleep(100 * time.Millisecond)
+
+	// List ConfigMaps
+	configMaps, err := clientset.CoreV1().ConfigMaps(corev1.NamespaceAll).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		t.Fatalf("Failed to list ConfigMaps: %v", err)
+	}
+	assert.Equal(t, 3, len(configMaps.Items))
+
+	cmController.lock.RLock()
+	defer cmController.lock.RUnlock()
+
+	indexer := cmController.cmInformer.Informer().GetIndexer()
+	hubNames := indexer.ListIndexFuncValues(ByHub)
+	sort.Strings(hubNames)
+	assert.Equal(t, hubNames, []string{"mdaihub-first", "mdaihub-second", "mdaihub-third"})
+
+	hubMap := make(map[string]any)
+	for _, hubName := range hubNames {
+		objs, err := indexer.ByIndex(ByHub, hubName)
+		if err != nil {
+			continue
+		}
+		for _, obj := range objs {
+			cm := obj.(*corev1.ConfigMap)
+			hubMap[hubName] = cm
+		}
+	}
+	assert.Equal(t, hubMap["mdaihub-first"], configMap1)
+	assert.Equal(t, hubMap["mdaihub-second"], configMap2)
+	assert.Equal(t, hubMap["mdaihub-third"], configMap3)
+
+}
